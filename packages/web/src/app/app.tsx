504cd508e896aa857011b1c12942364eb2f2e9fd
HallexCosta <hallex.costa@hotmail.com>
2023-02-06 01:06:18 -0300

    refactor: send request only close signIn modal
    Was adjusted:
    - send user token together with requests

--- packages/web/src/app/app.tsx
+++ packages/web/src/app/app.tsx
@@ -1,18 +1,30 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useEffect, useRef, useState } from 'react'
 import '../lib/dayjs'
 import '../styles/global.css'
 
 import { Header } from './components/Header'
 import { SummaryTable } from './components/SummaryTable'
 
+import { SignInModal } from './components/SignInModal'
+
 export function App() {
+  const [isOpenModal, setIsOpenModal] = useState(true)
+
+  function handleToggleIsOpenModal(stateModal: boolean) {
+    setIsOpenModal(stateModal)
+    console.log('isOpenedModal')
+  }
+
   return (
     <div className="w-screen h-screen flex justify-center items-center text-white">
+      <SignInModal onChangeIsOpenModal={handleToggleIsOpenModal} />
+
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
 
-        <SummaryTable />
+        <SummaryTable isOpenModal={isOpenModal} />
       </div>
     </div>
-  );
+  )
 }
--- packages/web/src/app/components/SummaryTable.tsx
+++ packages/web/src/app/components/SummaryTable.tsx
@@ -1,69 +1,79 @@
-import dayjs from "dayjs"
-import { useEffect, useState } from "react"
-import { api, HTTPResponse, SummaryResponseAPI } from "../../lib/axios"
-import { generateDatesFromBeginning } from "../utils/generate-dates-from-beginning"
-import { HabitDay } from "./HabitDay"
-import { WeekDay } from "./WeekDay"
+import dayjs from 'dayjs'
+import { useEffect, useState } from 'react'
+import { api } from '../../lib/axios'
+import { Summary } from '../domains'
 
-const weekDays = [
-  'D',
-  'S',
-  'T',
-  'Q',
-  'Q',
-  'S',
-  'S'
-]
+import { AxiosResponseSuccessAdapter } from '../../lib/axios/axios-interceptor-response-adapter'
+import { generateDatesFromBeginning } from '../utils/generate-dates-from-beginning'
+import { getLocalStorageData } from '../utils/get-local-storage-data'
+import { HabitDay } from './HabitDay'
+import { WeekDay } from './WeekDay'
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 const summaryDates = generateDatesFromBeginning()
 const minimiumSummaryDatesSize = 18 * 7 // 18 weeks
 const amountOfDaysToFill = minimiumSummaryDatesSize - summaryDates.length
 
-export function SummaryTable() {
-  const [summary, setSummary] = useState<SummaryResponseAPI>([])
+interface SummaryTableProps {
+  isOpenModal: boolean
+}
+export function SummaryTable({ isOpenModal }: SummaryTableProps) {
+  const [summary, setSummary] = useState<Summary>([])
 
   useEffect(() => {
-    api.get<SummaryResponseAPI>('summary')
-      .then((response: HTTPResponse<SummaryResponseAPI>) => setSummary(response.data))
-  }, [])
+    console.log('Estou sendo chamado')
+    if (!isOpenModal) {
+      console.log('Estou buscando o usuário')
+      const { token } = getLocalStorageData<UserLogged>('userLogged')
+
+      api
+        .get<Summary>('/summary', {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        })
+        .then((response: AxiosResponseSuccessAdapter<Summary>) =>
+          setSummary(response.data)
+        )
+    }
+  }, [isOpenModal])
 
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
         {weekDays.map((weekDay, index) => (
-          <WeekDay 
-            key={`${weekDay}-${index}`}
-          >
-            {weekDay}
-          </WeekDay>
+          <WeekDay key={`${weekDay}-${index}`}>{weekDay}</WeekDay>
         ))}
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {/* Only execute when terminated API request */}
-        {summary.length > 0 && summaryDates.map(date => {
-          const dayInSummary = summary.find(day => {
-            return dayjs(date).isSame(day.date, 'day')
-          })
+        {summary.length > 0 &&
+          summaryDates.map((date) => {
+            const dayInSummary = summary.find((day) => {
+              return dayjs(date).isSame(day.date, 'day')
+            })
 
-          return (
-            <HabitDay 
-              key={date.toString()}
-              date={date}
-              amount={dayInSummary?.amount}
-              completed={dayInSummary?.completed}
-            />
-          )
-        })}
+            return (
+              <HabitDay
+                key={date.toString()}
+                date={date}
+                amount={dayInSummary?.amount}
+                completed={dayInSummary?.completed}
+              />
+            )
+          })}
 
-        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, index) => {
-          return (
-            <div 
-              key={index}
-              className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
-            />
-          )
-        })}
+        {amountOfDaysToFill > 0 &&
+          Array.from({ length: amountOfDaysToFill }).map((_, index) => {
+            return (
+              <div
+                key={index}
+                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+              />
+            )
+          })}
       </div>
     </div>
   )
