245a2eab80b0bd62703d92ad09bfffc6d5f4447b
HallexCosta <hallex.costa@hotmail.com>
2023-02-05 22:33:26 -0300

    style: indent HabitDay

--- packages/web/src/app/components/HabitDay.tsx
+++ packages/web/src/app/components/HabitDay.tsx
@@ -1,7 +1,6 @@
 import * as Popover from '@radix-ui/react-popover'
 import clsx from 'clsx'
 import dayjs from 'dayjs'
-import { setUncaughtExceptionCaptureCallback } from 'process'
 import { useState } from 'react'
 import { generateProgressPercentage } from '../utils/generate-progress-percentage'
 
@@ -20,7 +19,7 @@ interface HabitProps {
   title: string
 }
 
-export function HabitDay({ completed = 0, amount  = 0, date }: Props) {
+export function HabitDay({ completed = 0, amount = 0, date }: Props) {
   const completedPercentage = generateProgressPercentage(amount, completed)
 
   const [upProgress, setUpProgress] = useState(completedPercentage)
@@ -35,21 +34,30 @@ export function HabitDay({ completed = 0, amount  = 0, date }: Props) {
 
   return (
     <Popover.Root>
-      <Popover.Trigger 
-        className={clsx('w-10 h-10 border-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-[#09090A]', {
-          'bg-zinc-900  border-zinc-800': upProgress === 0,
-          'bg-violet-500/80 border-violet-500/25': upProgress > 0 && upProgress < 20,
-          'bg-violet-600/80 border-violet-600/25': upProgress >= 20 && upProgress < 40,
-          'bg-violet-700/80 border-violet-700/25': upProgress >= 40 && upProgress < 60,
-          'bg-violet-800/80 border-violet-800/25': upProgress >= 60 && upProgress < 80,
-          'bg-violet-900/80 border-violet-900/25': upProgress >= 80
-        })}
+      <Popover.Trigger
+        className={clsx(
+          'w-10 h-10 border-2 rounded-lg transition-colors focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-[#09090A]',
+          {
+            'bg-zinc-900  border-zinc-800': upProgress === 0,
+            'bg-violet-500/80 border-violet-500/25':
+              upProgress > 0 && upProgress < 20,
+            'bg-violet-600/80 border-violet-600/25':
+              upProgress >= 20 && upProgress < 40,
+            'bg-violet-700/80 border-violet-700/25':
+              upProgress >= 40 && upProgress < 60,
+            'bg-violet-800/80 border-violet-800/25':
+              upProgress >= 60 && upProgress < 80,
+            'bg-violet-900/80 border-violet-900/25': upProgress >= 80,
+          }
+        )}
       />
 
       <Popover.Portal>
         <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col text-white focus:outline-none focus:ring-2 focus:ring-violet-700 focus:ring-offset-2 focus:ring-offset-[#09090A]">
           <span className="font-semibold text-zinc-400">{dayOfWeek}</span>
-          <span className="mt-1 font-extrabold leading-tight text-3xl">{dayAndMonth}</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">
+            {dayAndMonth}
+          </span>
 
           <ProgressBar progress={upProgress} />
 
