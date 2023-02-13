baf1b010eb7bd047a463718ed1a13ee36bc580a2
HallexCosta <hallex.costa@hotmail.com>
2023-02-13 12:12:16 -0300

    feat(web): add prefix when find user-logged on localStorage

--- apps/web/src/app/utils/get-local-storage-data.ts
+++ apps/web/src/app/utils/get-local-storage-data.ts
@@ -1,6 +1,17 @@
-type LocalStorageKeys = 'userLogged'
+type LocalStorageKeys = 'user-logged'
 
 export function getLocalStorageData<T>(key: LocalStorageKeys): T {
-  const data = localStorage.getItem(key) || '{}'
+  const prefix = '@habits-tracker'
+  const keyWithPrefix = `${prefix}:${key}`
+  const data = localStorage.getItem(keyWithPrefix) || '{}'
   return JSON.parse(data) as T
+}
+
+export function setLocalStorageData<T extends {}>(
+  key: LocalStorageKeys,
+  data: T
+) {
+  const prefix = '@habits-tracker'
+  const keyWithPrefix = `${prefix}:${key}`
+  localStorage.setItem(keyWithPrefix, JSON.stringify(data))
 }
