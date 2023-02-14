ca4fbb13bfc7edcc83821b03bfde0bd73b94da6a
HallexCosta <hallex.costa@hotmail.com>
2023-02-14 16:27:35 -0300

    chore(web): export AxiosInterceptorResponseAdapter

--- apps/web/src/lib/axios/index.ts
+++ apps/web/src/lib/axios/index.ts
@@ -1,6 +1,8 @@
 import { AxiosAdapter } from './axios-adapter'
 import axios from 'axios'
 
+export * from './axios-interceptor-response-adapter'
+
 const axiosAdapter = new AxiosAdapter(axios)
 console.log(import.meta.env.VITE_API_HOST)
 export const api = axiosAdapter.create({
