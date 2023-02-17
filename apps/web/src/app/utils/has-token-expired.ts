dd512aa4e41f180292353abf3d454504dc55fd5a
HallexCosta <hallex.costa@hotmail.com>
2023-02-17 20:47:22 -0300

    feat(web): handle with token base64 and base64URL

--- apps/web/src/app/utils/has-token-expired.ts
+++ apps/web/src/app/utils/has-token-expired.ts
@@ -1,8 +1,20 @@
+function handleEncodedPayload(encodedPayload: string) {
+  const regex = /-/g
+  const regex2 = /_/g
+
+  if (regex.test(encodedPayload) || regex2.test(encodedPayload))
+    return decodeURIComponent(
+      escape(atob(encodedPayload.replace(regex, '+').replace(regex2, '/')))
+    )
+
+  return decodeURIComponent(escape(atob(encodedPayload)))
+}
+
 export function hasTokenExpired(token: string) {
   if (!token) return true
   const now = +new Date() / 1000
   const userInBase64 = token.split('.')[1]
-  const userInJSON = atob(userInBase64)
+  const userInJSON = handleEncodedPayload(userInBase64)
   const user = JSON.parse(userInJSON)
   if (user.exp < now) {
     console.log('Token expirou')
