46feee289643b9bfc58813895d90412b9bb097fb
HallexCosta <hallex.costa@hotmail.com>
2023-02-20 09:44:08 -0300

    chore(server): show firebase credentials

--- apps/server/src/lib/firebase.ts
+++ apps/server/src/lib/firebase.ts
@@ -4,7 +4,7 @@ import { UserRecord } from 'firebase-admin/auth'
 import { configs } from '../configs'
 
 const { firebase } = configs
-
+console.log(firebase.credential)
 admin.initializeApp({
   credential: admin.credential.cert(firebase.credential as unknown as string),
   databaseURL: firebase.databaseURL,
